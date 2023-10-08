e6a7cd10733e17e29a443d91114c1f0b7699fe85
kkamauj <johnn2k@proton.me>
2023-10-08 10:29:44 -0700

    3-name and 4-puts.c program added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This function prints a message to the console
+ *              using the `puts` function and returns 0 to indicate
+ *              successful execution.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
