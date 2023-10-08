6ac24055f65a0d1a67f34734d1497617fb766c68
kkamauj <johnn2k@proton.me>
2023-10-08 10:43:54 -0700

    5-print.c program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program uses the printf function
+ * to print a message to the console.
+ *
+ * Return: 0 : Always 0 when succeful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
