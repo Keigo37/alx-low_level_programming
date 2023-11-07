82e51228c23031c535faefd260d7d2d1155e7f54
kkamauj <kkamauj@proton.me>
2023-11-07 07:27:17 -0800

    add a header file dog.h with a struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - contains a struct with 3 members
+ * @name: first member of char type
+ * @age: second member of float type
+ * @owner: third member of char type
+ *
+ * Description: struct dog with 3 members
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
